0704b74a4e099ea7c7be15312f4b35803685fd0f
toye1990 <ifetoyeobafemi@gmail.com>
2024-04-09 15:03:26 +0100

    pushing ratings

--- app.js
+++ app.js
@@ -4,6 +4,11 @@ const buttons = document.querySelectorAll('.figures')
 const ratins = document.querySelector('#ratings')
 const submitButton = document.getElementById('submitMe')
 const rateNow = document.getElementById('submitMe2')
+const button2 = document.querySelector('.figures2')
+const button3 = document.querySelector('.figures3')
+const button4 = document.querySelector('.figures4')
+const button5 = document.querySelector('.figures5')
+const button1 = document.querySelector('.figures1')
 
 
 //add click event on submit button
@@ -23,3 +28,55 @@ buttons.forEach((button) =>{
        ratins.innerHTML = button.innerHTML
     })
 })
+
+button1.addEventListener('click', () =>{
+button1.style.backgroundColor = 'gray'
+if (button1.style.backgroundColor == 'gray') {
+ button2.style.backgroundColor =''
+ button3.style.backgroundColor =''
+ button4.style.backgroundColor =''
+ button5.style.backgroundColor =''
+}
+})
+
+button2.addEventListener('click', () =>{
+    button2.style.backgroundColor = 'gray'
+    if (button2.style.backgroundColor == 'gray') {
+     button1.style.backgroundColor =''
+     button3.style.backgroundColor =''
+     button4.style.backgroundColor =''
+     button5.style.backgroundColor =''
+    }
+    })
+
+button3.addEventListener('click', () =>{
+        button3.style.backgroundColor = 'gray'
+        if (button3.style.backgroundColor == 'gray') {
+         button1.style.backgroundColor =''
+         button2.style.backgroundColor =''
+         button4.style.backgroundColor =''
+         button5.style.backgroundColor =''
+    }
+ })
+    
+
+ button4.addEventListener('click', () =>{
+    button4.style.backgroundColor = 'gray'
+    if (button4.style.backgroundColor == 'gray') {
+     button1.style.backgroundColor =''
+     button2.style.backgroundColor =''
+     button3.style.backgroundColor =''
+     button5.style.backgroundColor =''
+}
+})
+
+button5.addEventListener('click', () =>{
+    button5.style.backgroundColor = 'gray'
+    if (button5.style.backgroundColor == 'gray') {
+     button1.style.backgroundColor =''
+     button2.style.backgroundColor =''
+     button3.style.backgroundColor =''
+     button4.style.backgroundColor =''
+}
+})
+
